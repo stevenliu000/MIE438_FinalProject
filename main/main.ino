94d366ef2d429369da6cb61816e017adda5943b6
cheneyuwu <cheneyuwu@gmail.com>
2018-04-01 05:41:11 -0400

    1

--- main/main.ino
+++ main/main.ino
@@ -3,7 +3,6 @@
 #include <Servo.h>
 #include <Wire.h> 
 #include <Adafruit_L3GD20.h>
-
 AlphaBot Car1 = AlphaBot();
 Servo myservo; 
 
@@ -12,8 +11,8 @@ SoftwareSerial HM10(0,1); // RX, TX
 int leftWheel = 2, rightWheel = 3;
 int ECHO = 12, TRIG = 11;
 int SERVO = 10;
-#define GYRO_CS A1 // labeled CS
-#define GYRO_DO A0 // labeled SA0
+#define GYRO_CS 7 // labeled CS
+#define GYRO_DO 8 // labeled SA0
 #define GYRO_DI A4  // labeled SDA
 #define GYRO_CLK A5 // labeled SCL
 Adafruit_L3GD20 gyro(GYRO_CS, GYRO_DO, GYRO_DI, GYRO_CLK);
@@ -35,9 +34,7 @@ int servoAngle = 0;
 //gyro sensor
 double gyroAngle = 0;
 
-
 //------------------------------------------------------------------------------
-
 
 // Software setup-------------------------------------------------------------
 void UltrasonicConfig()
@@ -71,24 +68,32 @@ bool ServoToTargetAngle(int angle);
 void ultrasonicDistanceUpdate();
 int AngleSweep();
 inline void GyroToZero();
-void GyroAngleUpdate(double parameter);
+inline void GyroAngleUpdate(double parameter);
+bool cartRotate(int angle);
 //------------------------------------------------------------------------------------
 
 
 //main loop-----------------------------------------------------------------------
 void loop() { 
+  int targetAngle;
   //state 1 Waiting for Start Command 
   ServoTo90();
-  ServoToTargetAngle(20);
   bluetoothReadCommand('s');
 
-  //int angle = AngleSweep();
-  //Serial.println(angle);
-  //ServoToTargetAngle(angle);
-  //bluetoothReadCommand('s');
+  //state 2 start to find the bocket
+  cartRotate(90-AngleSweep());
+  cartRotate(90-AngleSweep());
+  
+  //state 3 move to the bocket
+  straightFollowing(20);
+  
+  //state 4 scan the bocket
+  cartRotate(-80);
+  ServoToTargetAngle(20);
   wallFollowing();
-  //ultrasonicDistanceUpdate();
-  //Serial.println(ultrasonicDistance);
+
+  //state 5 rotate towards next bocket
+  cartRotate(-80);
 }
 //------------------------------------------------------------------------------------
 
@@ -105,21 +110,27 @@ int AngleSweep() {
     ultrasonicDistanceUpdate();
     distanceArray[pos] = ultrasonicDistance;
     servoAngle = pos;
-    delay(15);                       // waits 15ms for the servo to reach the position
+    delay(10);                       // waits 15ms for the servo to reach the position
   }
   ServoToTargetAngle(90);
   for (pos = 0; pos<=180;pos++){
     Serial.println(distanceArray[pos]);
   }
-  for(pos = 1; pos<= 180; pos+= 1){
-    if (distanceArray[pos]-distanceArray[pos-1]>40){
-      endAngle = pos;
+  Serial.write("lasdfjlasdlkfas");
+    for(pos = 2; pos<= 180; pos+= 1){
+    if (distanceArray[pos]-distanceArray[pos-2]>30){
+      endAngle = pos-2;
+      Serial.println(distanceArray[pos]);
+      Serial.println(distanceArray[pos-2]);
     }
-    else if (distanceArray[pos]-distanceArray[pos-1]<-40){
+    else if (distanceArray[pos]-distanceArray[pos-2]<-30){
       startAngle = pos;
+      Serial.println(distanceArray[pos]);
+      Serial.println(distanceArray[pos-2]);
     }
   }
-  Serial.println(endAngle);
+
+   Serial.println(endAngle);
    Serial.println(startAngle);
     return (endAngle+startAngle)/2;
 }
@@ -130,57 +141,89 @@ bool wallFollowing(){
  double lastError = 0;
  double error = 0;
  double control;
- double kp=1,ki=0,kd=0;
+ double kp=1,ki=0.01,kd=0.1;
  double integral, derivative;
- double desiredDistance = 15;
-
- delay(1000);
- Car1.MotorRun(120,150);       //Car turn right for 1s; left motor speed:250,right motor speed:0
- for (int m = 0;m<200 ;m++){
+ double desiredDistance = 20;
+ GyroToZero();
+ delay(100);
+ Car1.MotorRun(180,100);       //Car turn right for 1s; left motor speed:250,right motor speed:0
+ while(gyroAngle<540){
   ultrasonicDistanceUpdate();
   error = ultrasonicDistance - desiredDistance;
   integral = (error+lastError)/2;
   derivative = error - lastError;
   control = error*kp + integral*ki + derivative*kd;
   lastError = error;
-  control = min(control,50);
-  Serial.println(control);
+  control = min(control,35);
   Car1.MotorRun(180+control,100-control);       //Car turn right for 1s; left motor speed:250,right motor speed:
   delay(100);
+  GyroAngleUpdate(0.117);
  }
+  Car1.MotorRun(-100,-100);
+  delay(100); 
   Car1.Brake();
 }
 
-bool straightFollowing(){
+bool cartRotate(int angle){
+ GyroToZero();
+ delay(100);
+ if (abs(angle)<5) return 0;
+ if (angle < 0){
+   Car1.MotorRun(-125,125);       //Car turn right for 1s; left motor speed:250,right motor speed:0
+   while(gyroAngle> (angle+5)){
+   //Car1.MotorRun(-125,125);       //Car turn right for 1s; left motor speed:250,right motor speed:
+    delay(100);
+    GyroAngleUpdate(0.117);
+ }
+  Car1.MotorRun(100,-100);
+  delay(100); 
+  Car1.Brake();
+ }
+ else{
+   Car1.MotorRun(125,-125);       //Car turn right for 1s; left motor speed:250,right motor speed:0
+   while(gyroAngle< (angle-5)){
+  //Car1.MotorRun(125,-125);       //Car turn right for 1s; left motor speed:250,right motor speed:
+  delay(100);
+  GyroAngleUpdate(0.117);
+ }
+  Car1.MotorRun(-100,100);
+  delay(100); 
+  Car1.Brake();
+ }
+
+}
+
+bool straightFollowing(int targetDistance){
  int flag = 0;
  int i,j;
  double lastError = 0;
  double Error = 0;
  double Control;
- double kp=1,ki=0.01,kd=1;
+ double kp=5,ki=0,kd=1;
  double integral, derivative;
- 
- delay(1000);
+ultrasonicDistanceUpdate(); 
+ //Serial.println(ultrasonicDistance);
+ delay(100);
  currLeft = digitalRead(leftWheel);
  currRight = digitalRead(rightWheel);
- Car1.MotorRun(120,120);       //Car turn right for 1s; left motor speed:250,right motor speed:0
- delay(1000);
- for (int m = 0;m<80 ;m++){  
+ Car1.MotorRun(150,150);       //Car turn right for 1s; left motor speed:250,right motor speed:0
+ while (ultrasonicDistance>targetDistance){
+  ultrasonicDistanceUpdate(); 
   for (int t = 0;t<100;t++){
     delay(1);
     updateDistance();
     }
-  Error = leftDistance*5-rightDistance*5;
+  Error = leftDistance-rightDistance;
   integral = (Error+lastError)/2;
   derivative = Error - lastError;
   Control = Error*kp + integral*ki + derivative*kd;
   lastError = Error;
-  Car1.MotorRun(120-Control,120+Control);       //Car turn right for 1s; left motor speed:250,right motor speed:
- }
-  Serial.println(leftDistance);
-  Serial.println(rightDistance);
+  //Serial.println(Control);
+  Car1.MotorRun(160-Control,150+Control);       //Car turn right for 1s; left motor speed:250,right motor speed:
+}
+  Car1.MotorRun(-100,-100);
+  delay(100); 
   Car1.Brake();
-  delay(5000);
   leftDistance = 0;
   rightDistance = 0;
 }
@@ -189,10 +232,9 @@ inline void GyroToZero(){
    gyroAngle = 0;
 }
 
-void GyroAngleUpdate(double parameter){
+inline void GyroAngleUpdate(double parameter){
   gyro.read();
   gyroAngle += (parameter)*((int)gyro.data.z-1);
-  delay(100);
 }
 
 void ultrasonicDistanceUpdate()                      // Measure the distance 
@@ -251,14 +293,14 @@ bool ServoToTargetAngle(int angle){
   if (servoAngle <= angle){
     for (pos = servoAngle; pos <= angle; pos += 1) {
       myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15ms for the servo to reach the position
+      delay(5);                       // waits 15ms for the servo to reach the position
     }
     servoAngle = angle;
   }
   else{
     for (pos = servoAngle; pos >= angle; pos -= 1) {
       myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15ms for the servo to reach the position
+      delay(5);                       // waits 15ms for the servo to reach the position
     }
     servoAngle = angle;
   }
